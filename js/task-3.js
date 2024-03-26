b3a61e9ff143d4e82ba15f03f45aeace310566e6
Yuliia <kotyuliya24@gmail.com>
2024-03-25 21:05:59 -0400

    task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    const amount = this.playTime + hours;
+  },
+
+  getInfo() {
+    return `${Username} has ${amount} active hours!`;
+  },
+};
+
